f123aea18bf69efb0765b54dbd570d12f4127607
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2016-01-18 19:30:01 -0800

    Created an empty Android application with a blank activity.

---
+++ ClassPlanner/app/src/androidTest/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/ApplicationTest.java
@@ -0,0 +1,13 @@
+package io.github.cse110w260t13.ucsdcse110wi16.classplanner;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
