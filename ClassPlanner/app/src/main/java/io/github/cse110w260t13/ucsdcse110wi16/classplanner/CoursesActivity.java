238309a1f03f6f557e8bd661bf8abf75539ead49
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2016-01-22 14:41:14 -0800

    Added FABs to calendar and course activities, as well as backstack buttons.

--- ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/CoursesActivity.java
+++ ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/CoursesActivity.java
@@ -1,7 +1,12 @@
 package io.github.cse110w260t13.ucsdcse110wi16.classplanner;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class CoursesActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class CoursesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.courses_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "Add a new course", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/MainActivity.java
+++ ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.main_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
