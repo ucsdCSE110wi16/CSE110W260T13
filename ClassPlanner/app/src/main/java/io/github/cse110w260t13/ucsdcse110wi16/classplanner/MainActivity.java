af0f1ccc9f25817bdb5a41b59f1b50195c031f9f
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2016-01-22 21:26:05 -0800

    Removed the pesky 'toolbar' in MainActivity because it doesn't really look like an actionbar.

--- ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/MainActivity.java
+++ ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/MainActivity.java
@@ -21,8 +21,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.main_fab);
         fab.setOnClickListener(new View.OnClickListener() {
