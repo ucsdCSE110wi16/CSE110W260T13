2f490a6528613e693f6b6c952d22ae10c4ad1cbb
Jessica Ng <jen030@ucsd.edu>
2016-02-10 19:18:22 -0800

    Just setSupportActionBar(toolbar) in main to fix unchanging toolbar name

--- ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/fragments/Calendar/CalendarFragment.java
+++ ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/fragments/Calendar/CalendarFragment.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
