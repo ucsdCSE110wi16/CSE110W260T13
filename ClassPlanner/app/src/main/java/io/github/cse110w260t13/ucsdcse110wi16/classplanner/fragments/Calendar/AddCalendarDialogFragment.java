51bc8a9d58407c255090e1f1b62eb6471d7bbd82
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2016-02-19 23:27:43 -0800

    Fixed the calendar dialog.

--- ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/fragments/Calendar/AddCalendarDialogFragment.java
+++ ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/fragments/Calendar/AddCalendarDialogFragment.java
@@ -2,11 +2,19 @@ package io.github.cse110w260t13.ucsdcse110wi16.classplanner.fragments.Calendar;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.text.Layout;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 
 import io.github.cse110w260t13.ucsdcse110wi16.classplanner.R;
 
@@ -15,15 +23,22 @@ import io.github.cse110w260t13.ucsdcse110wi16.classplanner.R;
  */
 public class AddCalendarDialogFragment extends android.support.v4.app.DialogFragment {
 
+    private LinearLayout calendarDialogLayout;
+    private CheckBox repeatCheckBox;
+
+    private View repeatView;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
+        View view = inflater.inflate(R.layout.dialog_add_calendar_item, null);
+
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        builder.setView(inflater.inflate(R.layout.dialog_add_calendar_item, null))
+        builder.setView(view)
                 // Add action buttons
                 .setPositiveButton("submit", new DialogInterface.OnClickListener() {
                     @Override
@@ -41,8 +56,45 @@ public class AddCalendarDialogFragment extends android.support.v4.app.DialogFrag
                         AddCalendarDialogFragment.this.getDialog().cancel();
                     }
                 })
-                .setTitle("Title");
-        return builder.create();
+                .setTitle("Add event");
+
+        Dialog dialog = builder.create();
+
+        // Get the linear layout
+        calendarDialogLayout = (LinearLayout) view.findViewById(R.id.calendar_dialog_linear_layout);
+
+        // Get the checkbox
+        repeatCheckBox = (CheckBox) view.findViewById(R.id.calendar_repeat_checkbox);
+
+        // get the repeating layout view
+        LayoutInflater layoutInflater = (LayoutInflater)
+                getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        repeatView = (View) layoutInflater.inflate(
+                R.layout.dialog_add_calendar_item_repeat,
+                (ViewGroup) calendarDialogLayout.getParent(),
+                false);
+
+        // Set the checkbox's on change listener
+        repeatCheckBox.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        // show or hide the included view
+                        if (isChecked) {
+                            LayoutInflater layoutInflater = (LayoutInflater)
+                                    getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                            calendarDialogLayout.addView(repeatView);
+                        } else {
+                            ((ViewGroup) repeatView.getParent()).removeView(repeatView);
+
+                        }
+
+                    }
+                }
+        );
+
+        return dialog;
     }
 
 }
