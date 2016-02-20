14a18927a4253c6f88850c9d3cc51af1933c60fe
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2016-02-19 23:38:16 -0800

    Minor change to keyboard handling on calendar dialog title.

--- ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/fragments/Calendar/AddCalendarDialogFragment.java
+++ ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/fragments/Calendar/AddCalendarDialogFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
