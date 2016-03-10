f17575bb04af69abfce09dfc2c64e476e3008d43
yuw178 <yuw178@ucsd.edu>
2016-03-10 01:00:34 -0800

    Commented CalendarEvent.java

--- ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/fragments/Calendar/EventUtil/CalendarEvent.java
+++ ClassPlanner/app/src/main/java/io/github/cse110w260t13/ucsdcse110wi16/classplanner/fragments/Calendar/EventUtil/CalendarEvent.java
@@ -1,5 +1,7 @@
 package io.github.cse110w260t13.ucsdcse110wi16.classplanner.fragments.Calendar.EventUtil;
-
+/**
+ * Class that represents calendar events.
+ */
 public class CalendarEvent {
     public String eventID;
     public String eventType;
@@ -7,7 +9,16 @@ public class CalendarEvent {
     public String eventDescr;
     public String eventSTime;
     public String eventETime;
-
+    
+    /**
+     * Calendar event constructor.
+     * @param id of the event
+     * @param type of event
+     * @param title of event
+     * @param descr description of the event
+     * @param sTime start time
+     * @param eTime end time
+     */
     public CalendarEvent(String id, String type, String title, String descr, String sTime, String eTime){
         eventID = id;
         eventType = type;
